1dc3684682136f3cbc3141a274f296caf5e96675
Madhan <madhan.adepu9@gmail.com>
2026-04-12 15:48:02 +0530

    build: fix gradle lifecycle error in root build file

--- app/android/build.gradle.kts
+++ app/android/build.gradle.kts
@@ -29,13 +29,9 @@ subprojects {
         targetCompatibility = "17"
     }
     
-    afterEvaluate {
-        if (project.hasProperty("kotlin")) {
-            tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-                compilerOptions {
-                    jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
-                }
-            }
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
         }
     }
 }
